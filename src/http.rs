7312d57a01f450af6f91aa9cee1ca9724d570df8
Lucas Dohmen <lucas@dohmen.io>
2018-04-19 16:26:17 +0200

    Implement /mine

--- src/http.rs
+++ src/http.rs
@@ -11,7 +11,7 @@ use chain::Block;
 #[serde(rename_all = "camelCase")]
 struct NodeInfo {
     node_id: String,
-    current_block_height: u32
+    current_block_height: u32,
 }
 
 #[derive(Serialize)]
@@ -19,6 +19,13 @@ struct NodeInfo {
 struct BlocksResponse {
     blocks: Vec<Block>,
     block_height: usize,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct MineResponse {
+    message: String,
+    block: Block,
 }
 
 pub struct Server {
@@ -30,7 +37,10 @@ impl Server {
     pub fn new() -> Server {
         let node_id = Uuid::new_v4().to_string();
         let rusty_chain: Vec<Block> = vec![Block::genesis()];
-        Server { node_id, rusty_chain }
+        Server {
+            node_id,
+            rusty_chain,
+        }
     }
 }
 
@@ -38,14 +48,36 @@ pub fn route(server: &Server, request: &Request) -> Response {
     match request.url().as_str() {
         "/" => node_info(server),
         "/blocks" => blocks(server),
-        _ => Response::text("not found").with_status_code(404)
+        "/mine" => mine(server),
+        _ => Response::text("not found").with_status_code(404),
     }
 }
 
 fn node_info(server: &Server) -> Response {
-    Response::json(&NodeInfo { node_id: server.node_id.clone(), current_block_height: 0 })
+    Response::json(&NodeInfo {
+        node_id: server.node_id.clone(),
+        current_block_height: 0,
+    })
 }
 
 fn blocks(server: &Server) -> Response {
-    Response::json(&BlocksResponse { blocks: server.rusty_chain.clone(), block_height: server.rusty_chain.len() })
+    Response::json(&BlocksResponse {
+        blocks: server.rusty_chain.clone(),
+        block_height: server.rusty_chain.len(),
+    })
+}
+
+fn mine(server: &Server) -> Response {
+    let previous_block = server.rusty_chain.last().unwrap();
+    let mut block = Block::new(vec![], previous_block);
+    let (nanos, hash_rate) = block.mine();
+    let message = format!(
+        "Mined a new block in {}ns. Hashing power: {} hashes/s.",
+        nanos, hash_rate
+    );
+
+    Response::json(&MineResponse {
+        block: block,
+        message: message,
+    })
 }
