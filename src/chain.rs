fd8310188b25b7d4ba3a13041c4e7dac849c6d7c
Joerg Mueller <joerg.mueller@innoq.com>
2018-04-19 15:28:39 +0200

    Creating a new Block.

--- src/chain.rs
+++ src/chain.rs
@@ -2,25 +2,41 @@ extern crate serde_json;
 extern crate crypto_hash;
 
 use crypto_hash::{Algorithm, hex_digest};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Transaction {
     id:String,
-    timestamp:u32,
+    timestamp:u64,
     payload:String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Block {
     index:u32,
-    timestamp:u32,
+    timestamp:u64,
     proof:u32,
     transactions:Vec<Transaction>,
     previous_block_hash:String,
 }
 
 impl Block {
+
+    pub fn new(transactions: Vec<Transaction>, previous_block: &Block) -> Block {
+
+        let now = SystemTime::now();
+        let duration_since_epoch = now.duration_since(UNIX_EPOCH).unwrap();
+
+        Block {
+            index: previous_block.index + 1,
+            timestamp: duration_since_epoch.as_secs(),
+            proof: 0,
+            transactions: transactions,
+            previous_block_hash: previous_block.hash(),
+        }
+    }
+
     pub fn genesis() -> Block {
         let transaction = Transaction{
             id: String::from("b3c973e2-db05-4eb5-9668-3e81c7389a6d"),
@@ -28,15 +44,13 @@ impl Block {
             timestamp: 0,
         };
 
-        let block = Block {
+       Block {
             index: 1,
             timestamp: 0,
             proof: 1917336,
             transactions: vec!(transaction),
             previous_block_hash: String::from("0"),
-        };
-
-        block
+        }
     }
 
     pub fn to_json(&self) -> String {
@@ -64,4 +78,14 @@ fn test_hash_for_genesis_block() {
     let genesis = Block::genesis();
 
     assert_eq!("000000b642b67d8bea7cffed1ec990719a3f7837de5ef0f8ede36537e91cdc0e", genesis.hash())
+}
+
+#[test]
+fn test_new_empty_block() {
+    let previous_block = Block::genesis();
+    let block = Block::new(Vec::new(), &previous_block);
+
+    assert_eq!(block.transactions.len(), 0);
+    assert_eq!(block.index, 2);
+    assert_eq!(block.previous_block_hash, previous_block.hash());
 }
